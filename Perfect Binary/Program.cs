03adff7327c3c7593f02e0eb1242810bfb2591d0
Rudy Menjivar <ram302@nyu.edu>
2020-08-31 12:33:20 -0400

    Missed the try/catch block in code!

--- Perfect Binary/Program.cs
+++ Perfect Binary/Program.cs
@@ -8,10 +8,8 @@ namespace Perfect_Binary
         static void Main(string[] args)
         {
             string StringToCheck = "00111101011001";
-            Console.WriteLine("Count 0s: " + Regex.Matches(StringToCheck, "0").Count);
-            Console.WriteLine("Count 1s: " + Regex.Matches(StringToCheck, "1").Count);
-
-            Console.WriteLine("Is good: " + IsGoodBinaryString(StringToCheck));
+            
+            Console.WriteLine("Binary string " + StringToCheck + " status check: " + IsGoodBinaryString(StringToCheck));
             Console.ReadKey();
         }
 
@@ -35,30 +33,38 @@ namespace Perfect_Binary
             int ZeroCount = 0;
             int OneCount = 0;
 
-            foreach(char ch in BinStr.ToCharArray())
+            try
             {
-                if (ch == '0')
-                {
-                    ZeroCount++;
-                } 
-                else if(ch == '1')
-                {
-                    OneCount++;
-                }
-                else
-                {
-                    throw new System.FormatException("Invalid character found in incoming string. Found " + ch + " in string. Expecting only 0s and 1s.");
-                }
 
-                /*
-                 * The second condition requires that 0's should not outnumber 1's for any prefix. We could just return false
-                 * here, but for clarity we'll just break the loop.
-                */
-                if(ZeroCount > OneCount)
+                foreach (char ch in BinStr.ToCharArray())
                 {
-                    PrefixOnesNeverLessThanZeros = false;
-                    break;
+                    if (ch == '0')
+                    {
+                        ZeroCount++;
+                    }
+                    else if (ch == '1')
+                    {
+                        OneCount++;
+                    }
+                    else
+                    {
+                        throw new System.FormatException("Invalid character found in incoming string. Found " + ch + " in string. Expecting only 0s and 1s.");
+                    }
+
+                    /*
+                     * The second condition requires that 0's should not outnumber 1's for any prefix. We could just return false
+                     * here, but for clarity we'll just break the loop.
+                    */
+                    if (ZeroCount > OneCount)
+                    {
+                        PrefixOnesNeverLessThanZeros = false;
+                        break;
+                    }
                 }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Format exception found in input string!");
             }
 
             // Both conditions should be true to determine a good binary string:
